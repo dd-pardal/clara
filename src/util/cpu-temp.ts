3601e1005353263659c9787e6f36036096f1fe64
D. Pardal <dd_pardal@outlook.pt>
2021-10-30 13:20:32 +0100

    Added a way to get the CPU temperature Currently only works on Raspberry Pi OS.

---
+++ src/util/cpu-temp.ts
@@ -0,0 +1,39 @@
+import { execFile } from "child_process";
+
+const getCPUTempFunctionPromise: Promise<() => Promise<number>> = new Promise((res, rej) => {
+	if (process.platform === "linux") {
+		// Check for Raspberry Pi OS
+		execFile("which", ["vcgencmd"], (err, stdout) => {
+			console.log(stdout);
+			if (err) {
+				if (err.killed === false && err.code === 1) {
+					rej(new TypeError("Unsupported OS."));
+				} else {
+					rej(err);
+				}
+			} else {
+				const vcgencmdPath = stdout.slice(0, -1);
+				res(() => new Promise((res, rej) => {
+					execFile(vcgencmdPath, ["measure_temp"], (err, stdout) => {
+						if (err) {
+							rej(err);
+						} else {
+							const temp = Number.parseFloat(stdout.slice(5, -3));
+							if (isNaN(temp)) {
+								rej(new Error("It wasn't possible to get the temperature."));
+							} else {
+								res(temp);
+							}
+						}
+					});
+				}));
+			}
+		});
+	} else {
+		rej(new TypeError("Unsupported OS."));
+	}
+});
+
+export async function getCPUTemp(): Promise<number> {
+	return (await getCPUTempFunctionPromise)();
+}
