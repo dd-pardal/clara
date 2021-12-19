ca8c2057da3d3447adf5db8b32b5e64500df62f3
D. Pardal <dd_pardal@outlook.pt>
2021-12-19 19:00:48 +0000

    Added a way to fetch YouTube channel data

---
+++ src/youtube/types.ts
@@ -0,0 +1,12 @@
+export interface PartialVideoData {
+	videoID: string;
+	title: string;
+}
+export interface ChannelData {
+	channelID: string;
+	name: string;
+	description: string;
+	profilePictureURL: string;
+	bannerURL: string | null;
+	newestVideos: PartialVideoData[];
+}
